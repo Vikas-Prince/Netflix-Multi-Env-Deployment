573182eb1482003b8fe5374aa24a363c4b032055
Vikas-Prince <vikasprince30809@gmail.com>
2024-11-24 16:53:01 +0530

    Implemented Header

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Nav from "../nav/Nav";
+import Header from "../header/Header";
 export default function Home() {
   return (
     <div>
       <Nav />
+      <Header />
     </div>
   );
 }
